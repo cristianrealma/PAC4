afe17c16c29503e75ed5d1b16921b2d3c243ecc4
cristianrealma <cristian_real97@hotmail.com>
2024-06-22 00:44:55 +0200

    Update main.py

--- pac/src/main.py
+++ pac/src/main.py
@@ -1,7 +1,16 @@
 # src/main.py
 
 import argparse
-from data_processing import read_csv, clean_csv, rename_col, breakdown_date, erase_month
+from data_processing import (
+    read_csv,
+    clean_csv,
+    rename_col,
+    breakdown_date,
+    erase_month,
+    groupby_state_and_year,
+    print_biggest_handguns,
+    print_biggest_longguns
+)
 
 def main():
     parser = argparse.ArgumentParser(description="Anàlisi de dades d'ús d'armes de foc als EUA")
@@ -11,40 +20,53 @@ def main():
     parser.add_argument('--rename_col', type=str, help='Canvia el nom de la columna longgun per long_gun')
     parser.add_argument('--breakdown_date', type=str, help='Divideix la columna month en year i month')
     parser.add_argument('--erase_month', type=str, help='Elimina la columna month')
+    parser.add_argument('--groupby_state_and_year', type=str, help='Agrupa el dataframe per estat i any')
+    parser.add_argument('--print_biggest_handguns', type=str, help='Imprimeix l\'estat i l\'any amb el nombre més gran de hand_guns')
+    parser.add_argument('--print_biggest_longguns', type=str, help='Imprimeix l\'estat i l\'any amb el nombre més gran de long_guns')
 
     args = parser.parse_args()
 
     if args.all or args.read_csv:
-        # Llegir el fitxer CSV
         if args.read_csv:
             filepath = args.read_csv
         else:
-            filepath = '../data/nics-firearm-background-checks.csv'  # Substituir per la ruta correcta
+            filepath = '../data/nics-firearm-background-checks.csv'
         df = read_csv(filepath)
 
     if args.all or args.clean_csv:
-        # Netejar el dataset
         if not args.all:
-            df = read_csv(args.clean_csv)  # Llegir el fitxer si només s'executa clean_csv
+            df = read_csv(args.clean_csv)
         df_cleaned = clean_csv(df)
 
     if args.all or args.rename_col:
-        # Canviar el nom de la columna
         if not args.all:
-            df = read_csv(args.rename_col)  # Llegir el fitxer si només s'executa rename_col
+            df = read_csv(args.rename_col)
         df_cleaned = rename_col(df)
 
     if args.all or args.breakdown_date:
-        # Dividir la columna month en year i month
         if not args.all:
-            df = read_csv(args.breakdown_date)  # Llegir el fitxer si només s'executa breakdown_date
+            df = read_csv(args.breakdown_date)
         df_cleaned = breakdown_date(df)
 
     if args.all or args.erase_month:
-        # Eliminar la columna month
         if not args.all:
-            df = read_csv(args.erase_month)  # Llegir el fitxer si només s'executa erase_month
+            df = read_csv(args.erase_month)
         df_cleaned = erase_month(df)
+
+    if args.all or args.groupby_state_and_year:
+        if not args.all:
+            df = read_csv(args.groupby_state_and_year)
+        df_grouped = groupby_state_and_year(df_cleaned)
+
+    if args.all or args.print_biggest_handguns:
+        if not args.all:
+            df_grouped = groupby_state_and_year(read_csv(args.print_biggest_handguns))
+        print_biggest_handguns(df_grouped)
+
+    if args.all or args.print_biggest_longguns:
+        if not args.all:
+            df_grouped = groupby_state_and_year(read_csv(args.print_biggest_longguns))
+        print_biggest_longguns(df_grouped)
 
 if __name__ == '__main__':
     main()
